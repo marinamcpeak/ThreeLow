5d5d4845558fe68df933f7d0f961407076258144
Marina <mmcpeak@live.ca>
2019-05-29 11:45:51 -0700

    Initial Commit

---
+++ ThreeLow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLow
+//
+//  Created by Marina Mona June McPeak on 2019-05-29.
+//  Copyright © 2019 Marina Mona June McPeak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
